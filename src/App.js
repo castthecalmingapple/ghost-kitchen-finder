f68ff1148399564a85a05e10f0f67c2a999e063c
castthecalmingapple <kyletucci@gmail.com>
2023-03-21 00:09:19 -0400

    Read info from JSON and mapped to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import kitchens from './data.json'
 
-function App() {
-  const data = fetch('restaurants.json')
-  const res = data.JSON()
-  console.log(res)
+function App() { 
+  console.log(kitchens)
   return (
     <div className="App">
+      {kitchens && kitchens.map(kitchen => {
+        const street = kitchen.address.street
+        const city = kitchen.address.city
+        const state = kitchen.address.state
+        const zipCode = kitchen.address.zipCode
+        const mainKitchen = kitchen.kitchens.main
+        const ghost1 = kitchen.kitchens.ghost1
+        const ghost2 = kitchen.kitchens.ghost2
+        const totalKitchens = kitchen.totalKitchens
 
+        return (
+          <>
+          <h1>{mainKitchen}</h1>
+          <h2>{`${street}, ${city} ${state}, ${zipCode}`}</h2>
+          <h1>{totalKitchens}</h1>
+          </>
+        )
+        })
+      }
     </div>
   );
 }
